b7e215a90b9e859dffcc0e73e9c384a60255f1f0
Christopher Jenner <49998052+ChrisJLKC@users.noreply.github.com>
2021-10-08 12:41:33 +0100

    Finished Task 303

--- Lab_1/TASK-303-PortOut/main.cpp
+++ Lab_1/TASK-303-PortOut/main.cpp
@@ -1,21 +1,18 @@
 #include "mbed.h"
 
 // Hardware Definitions
-#define TRAF_GRN1_PIN PC_6
-#define TRAF_YEL1_PIN PC_3
-#define TRAF_RED1_PIN PC_2
-#define LEDMASK 0b0000000001001100
+#define LEDMASK_C 0x4C // instead of writing out many 1 and 0's
+#define LEDMASK_B 0x4081
 // Objects
-//BusOut leds(TRAF_RED1_PIN, TRAF_YEL1_PIN, TRAF_GRN1_PIN);
-PortOut leds(PortC, LEDMASK);
+PortOut leds_C(PortC, LEDMASK_C);
+PortOut leds_B(PortB, LEDMASK_B);
 
 int main()
 {
     while (true) {
-        leds = 0;   //Binary 000
+        leds_C = leds_C ^ LEDMASK_C;   // Logic XOR switches on and off LED's
+        leds_B = leds_B ^ LEDMASK_B;
         wait_us(500000);
-        leds = 0xFF;   //Binary 11111111
-        wait_us(500000);    
     }
 }
 
