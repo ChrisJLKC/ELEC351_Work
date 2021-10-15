3dcdb4aed1de549084b5c2d108ffe516a8b001ff
Christopher Jenner <49998052+ChrisJLKC@users.noreply.github.com>
2021-10-15 10:48:23 +0100

    Added Task 311 and finished task 310

--- Lab_2/Task-310-Struct/main.cpp
+++ Lab_2/Task-310-Struct/main.cpp
@@ -6,7 +6,6 @@ typedef struct {
   double imag;
 } ComplexNumber_C;
 
-
 int main() {
 
     //Create instance of a complex number
@@ -19,10 +18,10 @@ int main() {
     //Create and Initialise 
     ComplexNumber_C q = {1.0, 1.0};
 
-    // TASK:
-    // Create another complex number y
-    // Calculate the complex sum of p and q and store in y
-    // Use printf to display as a complex number (hint: you need two placeholders)
+    // Creating instance of complex number and Initialise
+    ComplexNumber_C y = {p.real + q.real, p.imag + q.imag};
+
+    printf("%f %fi", y.real, y.imag);
     
     while (true) {
     }
